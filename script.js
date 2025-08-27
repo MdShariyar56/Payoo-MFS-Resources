7a080d43c538d2641d8dc940861a45fe74854b1c
MdShariyar56 <shahriyarkobir2005@gmail.com>
2025-08-27 11:16:20 +0600

    added a login page and home page

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("loginbutton").addEventListener("click",function(e){
+    e.preventDefault()
+    const mobileNumber = 01764841333
+    const pinNumber = 565656
+
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobileNumberValueConvertd = parseInt(mobileNumberValue)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt( pinNumberValue)
+
+    if(mobileNumberValueConvertd === mobileNumber &&  pinNumberValueConverted === pinNumber){
+        window.location.href="./home.html"    
+    }
+    else{
+        alert("Invalid")
+    }
+
+
+})   
